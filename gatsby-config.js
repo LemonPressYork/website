275a828ea91c2af085fd1d64e42a1db40b504446
Ben Silverman <bensilverman10@gmail.com>
2021-07-29 20:07:30 +0100

    Set up to pull from our Wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+          `http://lemonpress.yordevs.com/index.php?graphql`,
       },
     },
 
