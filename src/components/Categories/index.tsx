78ccb7bf78d98342fe1658825e7cf11208cec54f
Ben Silverman <bensilverman10@gmail.com>
2021-08-24 12:14:19 +0100

    Extract category data to separate file

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -3,20 +3,7 @@ import { styled } from "../../stitches.config";
 
 import { TextLink } from "../Link";
 
-type Category = {
-  id: string;
-  name: string;
-};
-
-const categories: Category[] = [
-  { name: "Campus", id: "campus" },
-  { name: "News & Politics", id: "news-and-politics" },
-  { name: "Lifestyle", id: "lifestyle" },
-  { name: "Science & Tech", id: "science-and-tech" },
-  { name: "Art", id: "art" },
-  { name: "Features", id: "features" },
-  { name: "Sports", id: "sports" },
-];
+import { categories } from "./data";
 
 const Bar = styled("div", {
   background: "$accentDark",
