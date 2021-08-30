58edf23213aae2c8651b600f9f3d5c499a60ffca
Adam Barr <48323401+4dambarr@users.noreply.github.com>
2021-08-30 16:06:19 +0100

    Setup about page (#32)

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { H1 } from "../components/Heading";
+import { Layout } from "../components/Layout";
+
+const About = () => {
+  return (
+    <Layout>
+      <H1>About The Lemon Press</H1>
+      <p>Page coming soon</p>
+    </Layout>
+  );
+};
+
+export default About;
