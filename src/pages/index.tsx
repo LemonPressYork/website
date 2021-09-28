fa3b787d55f4a244e51cbf42947f4d48b198d05d
4dambarr <ab2474@york.ac.uk>
2021-09-28 17:30:39 +0100

    Setup latest list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Container } from "../components/Container";
 import { Layout } from "../components/Layout";
 import { FeaturedPost } from "../components/FeaturedPost";
 import { CategoryGrid } from "../components/CategoryPost";
+import LatestIssueList from "../components/LatestArticlesList";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedPost />
       <Container>
         <CategoryGrid css={{ gridColumn: "1/8" }} />
+        <LatestIssueList css={{ gridColumn: "8/-1" }} />
       </Container>
     </Layout>
   );
