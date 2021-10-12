a0532b01e1e475ae19d272a3a4a144bbc9c9f920
4dambarr <ab2474@york.ac.uk>
2021-10-12 18:34:25 +0100

    Remove mobile version of latest issue

--- src/components/LatestIssue/index.tsx
+++ src/components/LatestIssue/index.tsx
@@ -10,10 +10,6 @@ const Wrapper = styled("div", {
   width: "300px",
 });
 
-const MobileWrapper = styled("div", {
-  width: "80%",
-});
-
 const monthNames = [
   "January",
   "February",
@@ -30,31 +26,17 @@ const monthNames = [
 ];
 
 export const LatestIssue = ({ css }) => {
-  const isDesktop = useMediaQuery(breakpoints.smUp);
-
   const { title, epoch, documentId, docname } = getLatestIssue();
 
   const date = new Date(epoch);
 
-  if (isDesktop) {
-    return (
-      <Wrapper style={css}>
-        <PrintIssueCard
-          title={title}
-          date={`${monthNames[date.getMonth()]} ${date.getFullYear()}`}
-          link={`https://issuu.com/thelemonpress/docs/${docname}`}
-          image={`https://image.issuu.com/${documentId}/jpg/page_1_thumb_large.jpg`}></PrintIssueCard>
-      </Wrapper>
-    );
-  } else {
-    return (
-      <MobileWrapper>
-        <PrintIssueCard
-          title={title}
-          date={`${monthNames[date.getMonth()]} ${date.getFullYear()}`}
-          link={`https://issuu.com/thelemonpress/docs/${docname}`}
-          image={`https://image.issuu.com/${documentId}/jpg/page_1_thumb_large.jpg`}></PrintIssueCard>
-      </MobileWrapper>
-    );
-  }
+  return (
+    <Wrapper style={css}>
+      <PrintIssueCard
+        title={title}
+        date={`${monthNames[date.getMonth()]} ${date.getFullYear()}`}
+        link={`https://issuu.com/thelemonpress/docs/${docname}`}
+        image={`https://image.issuu.com/${documentId}/jpg/page_1_thumb_large.jpg`}></PrintIssueCard>
+    </Wrapper>
+  );
 };
