0b80d807385946efeb6f49cb13a1a5cafc0e690a
4dambarr <ab2474@york.ac.uk>
2021-10-12 18:34:38 +0100

    Reformat landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,37 @@ import { FeaturedPost } from "../components/FeaturedPost";
 import { CategoryGrid } from "../components/CategoryPost";
 import { LatestIssue } from "../components/LatestIssue";
 import LatestArticleList from "../components/LatestArticlesList";
+import { useMediaQuery } from "../hooks/useMediaQuery";
+import { breakpoints } from "../utils";
 
 const Home = () => {
-  return (
-    <Layout>
-      <FeaturedPost />
-      <Container>
-        <CategoryGrid css={{ gridColumn: "1/8" }} />
-        <LatestArticleList css={{ gridColumn: "8/-1" }} />
-        <LatestIssue css={{ gridColumn: "10/-1" }} />
-      </Container>
-    </Layout>
-  );
+  const isDesktop = useMediaQuery(breakpoints.landingMid);
+
+  if (isDesktop) {
+    return (
+      <Layout>
+        <FeaturedPost />
+        <Container>
+          <CategoryGrid css={{ gridColumn: "1/8", gridRow: "1/4" }} />
+          <LatestArticleList css={{ gridColumn: "8/-1", gridRow: "1/8" }} />
+          <LatestIssue css={{ gridColumn: "10/-1" }} />
+        </Container>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <FeaturedPost />
+        <Container>
+          <CategoryGrid css={{ gridColumn: "1/-1" }} />
+          <LatestArticleList css={{ gridColumn: "1/-1" }} />
+          <LatestIssue
+            css={{ gridColumn: "1/-1", width: "100%", display: "flex", justifyContent: "center" }}
+          />
+        </Container>
+      </Layout>
+    );
+  }
 };
 
 export default Home;
